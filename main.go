c1c3db1f1e72848f535d5524b564f4281affc186
Brent Mitton <brent.mitton+github@gmail.com>
2023-06-30 00:44:01 +0000

    stubbing in some stuff

--- main.go
+++ main.go
@@ -1,17 +1,86 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
-	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
-func main() {
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
+var s *discordgo.Session
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading the .env file")
 	}
 
-	http.HandleFunc("/hello", helloHandler)
-	log.Println("Listing for requests at http://localhost:8000/hello")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	s, err = discordgo.New("Bot " + os.Getenv("DISCORD_BOT_TOKEN"))
+	if err != nil {
+		fmt.Println("Error creating Discord session:", err)
+		return
+	}
+}
+
+var commands = []*discordgo.ApplicationCommand{
+	// command with options
+	{
+		Name:        "create",
+		Description: "Kick off town creation",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "name",
+				Description: "The name of the town",
+				Required:    true,
+			},
+		},
+	},
+}
+
+var commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+	"create": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		townName := i.ApplicationCommandData().Options[0].StringValue()
+
+		// create the voice channels here
+
+	},
+}
+
+func init() {
+	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
+}
+
+func main() {
+
+	// Open a websocket connection to Discord and begin listening.
+	err := s.Open()
+	if err != nil {
+		fmt.Println("Error opening connection:", err)
+		return
+	}
+	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
+	for i, v := range commands {
+		cmd, err := s.ApplicationCommandCreate(s.State.User.ID, "", v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+		registeredCommands[i] = cmd
+	}
+
+	fmt.Println("Bot is running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL, os.Interrupt)
+	<-sc
+
+	s.Close()
+
 }
